38ebb0ca0dfe3b7bc0c391a39629cb823dc394cd
Benniton Otumfuo-Nyarko <benjiafriq@gmail.com>
2026-02-07 00:21:44 +0000

    Building more classes in storefront

--- store/models.py
+++ store/models.py
@@ -1,29 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Models):
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+class Product(models.Model):
     title = models.CharField(max_length=255) 
     descriptions = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
-class Order(models.Models):
-    PAYMENT_PENDING = 'P'
-    PAYMENT_COMPLETE = 'C'
-    PAYMENT_FAILED = 'F'
-
-    PAYMENT_CHOICES = [
-        ('PAYMENT_PENDING', 'P'),
-        ('PAYMENT_COMPLETE', 'C'),
-        ('PAYMENT_FAILED', 'F')
-    ]
-    placed_at = models.DateTimeField(auto_now_add=True)
-    payment_status = models.CharField()
-
-
-class Customer(models.Models):
-    MEMBERSHIP_BRONZE = 'B'
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B' 
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
 
@@ -38,9 +28,27 @@ class Customer(models.Models):
     email = models.EmailField(unique=True) # emailfield
     phone = models.CharField(max_length=255) 
     birth_date = models.DateField(null=True) # datefield
-    
+    customer = models.CharField(max_length=we are going to shool in )
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES)
 
+class Order(models.Model):
+    PAYMENT_PENDING = 'P'
+    PAYMENT_COMPLETE = 'C'
+    PAYMENT_FAILED = 'F'
 
+    PAYMENT_CHOICES = [
+        ('PAYMENT_PENDING', 'P'),
+        ('PAYMENT_COMPLETE', 'C'),
+        ('PAYMENT_FAILED', 'F')
+    ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_CHOICES, default=PAYMENT_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+
+class OrderItem
+
+class CartItem
+    
 
  
