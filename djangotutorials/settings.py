26705ec72b46c42a9c98720b6c55e9bfa90e223a
Benniton Otumfuo-Nyarko <benjiafriq@gmail.com>
2026-02-18 08:06:28 +0000

    Add comments for django apps

--- djangotutorials/settings.py
+++ djangotutorials/settings.py
@@ -31,14 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Default django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground',
+
+    # Extra
     'debug_toolbar',
+
+    # My apps
+    'playground',
     'store',
     'tags'
 ]
